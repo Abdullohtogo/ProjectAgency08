dc873a2edfc9ee9e4bfb677cfbcc2caf950c9a8b
Sardorbek <s.jabborov@uic.group>
2023-08-08 11:43:54 +0500

    feat: input focus style

--- components/Input/Input.vue
+++ components/Input/Input.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col gap-2">
     <label for="" class="text-gray-200">{{ label }}</label>
-    <div class="bg-gray-300 rounded-lg flex gap-2 p-3 h-full">
+    <div class="bg-gray-300 rounded-lg flex gap-2 p-3 h-full border border-transparent focus-within:border-green-400">
       <img :src="src" alt="">
       <span>
         <slot/>
       </span>
-      <input :type="type" name="" id="" class="outline-0 bg-transparent w-full" :maxlength="maxlength"
+      <input :type="type" name="" id="" class="outline-0 bg-transparent w-full caret-green-400" :maxlength="maxlength"
              :placeholder="placeholder"/>
     </div>
   </div>
