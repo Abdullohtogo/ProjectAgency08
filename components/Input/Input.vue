44a03f30c732fb849e3fd36c59da9a660e8e8aea
T.Bobur <b.turabov@imkon.uz>
2023-08-09 17:51:49 +0500

    feat: input validation on input

--- components/Input/Input.vue
+++ components/Input/Input.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="flex flex-col gap-2">
     <label class="text-gray-200">{{ label }}</label>
-    <div class="bg-gray-300 rounded-lg transition transition-300 flex sm:gap-2 gap-1 sm:p-3 p-1.5 h-full border border-transparent focus-within:border-green-400">
-      <img :src="src" alt="" v-if="src">
+    <div
+      :class="inputClass"
+      class="bg-gray-300 rounded-lg transition transition-300 flex sm:gap-2 gap-1 sm:p-3 p-1.5 h-full border border-transparent focus-within:border-green-400"
+    >
+      <img :src="src" alt="" v-if="src" />
       <span>
-        <slot/>
+        <slot />
       </span>
-      <input :type="type" class="outline-0 bg-transparent w-full caret-green-400 placeholder:text-sm" :maxlength="maxlength"
-             :placeholder="placeholder"/>
+      <input
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        :type="type"
+        class="outline-0 bg-transparent w-full caret-green-400 placeholder:text-sm"
+        :maxlength="maxlength"
+        :placeholder="placeholder"
+      />
     </div>
   </div>
 </template>
 <script setup lang="ts">
 interface Props {
-  label: string,
+  label: string
   placeholder: string
-  src: string,
+  src: string
   type: string
   maxlength: number
+  modelValue: any
 }
 
 defineProps<Props>()
