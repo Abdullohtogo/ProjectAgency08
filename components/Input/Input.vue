4ee9dd7588d467864b58cb1ef2ee53d8e0e47d05
T.Bobur <b.turabov@imkon.uz>
2023-08-15 15:56:03 +0500

    feat: phone input

--- components/Input/Input.vue
+++ components/Input/Input.vue
@@ -3,17 +3,17 @@
     <label class="text-gray-200">{{ label }}</label>
     <div
       :class="(inputClass, error ? '!border-red' : '')"
-      class="bg-gray-300 rounded-lg transition transition-300 flex sm:gap-2 gap-1 sm:p-3 p-1.5 h-full border border-transparent focus-within:border-green-400"
+      class="bg-gray-300 rounded-lg transition transition-300 flex sm:p-3 p-1.5 h-full border border-transparent focus-within:border-green-400"
     >
-      <img :src="src" alt="" v-if="src" />
-      <span class="text-sm text-black-100 leading-130 translate-y-[1px] font-normal flex items-center">
+      <img class="mr-2" :src="src" alt="" v-if="src" />
+      <span class="text-sm mr-1 text-black-100 leading-130  font-normal flex items-center">
         <slot />
       </span>
       <input
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         :type="type"
-        class="outline-0 bg-transparent w-full caret-green-400 placeholder:text-sm"
+        class="outline-0 text-sm text-black-100 leading-130 bg-transparent w-full caret-green-400 placeholder:text-sm"
         :maxlength="maxlength"
         :placeholder="placeholder"
       />
