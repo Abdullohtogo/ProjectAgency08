2bd1f2adec91032ed74e1cb066262c66f29488f3
Ibrohim Olimjonov <i.olimjonov@imkon.uz>
2023-08-16 16:26:24 +0500

    fix: advertisement

--- components/Common/Checkbox.vue
+++ components/Common/Checkbox.vue
@@ -18,9 +18,9 @@
     />
     <span class="pl-8 max-w-[301px] text-gray-200  sm:text-base text-sm leading-130">
       <slot class="">
-        <a href="" target="_blank" class="text-[#4489F7] font-medium">
+        <NuxtLink :href="url" target="_blank" class="text-[#4489F7] font-medium">
           {{ $t(labelStart) }}
-        </a>
+        </NuxtLink>
         {{ $t(label) }}
       </slot>
     </span>
