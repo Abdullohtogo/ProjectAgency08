ebb82f4cfd7a3bd5fdfcf2abcf73a5b9a16fb27e
T.Bobur <b.turabov@imkon.uz>
2023-08-22 16:22:37 +0500

    feat: checkbox animation remove

--- components/Common/Checkbox.vue
+++ components/Common/Checkbox.vue
@@ -13,7 +13,7 @@
       @change="handleChange"
     />
     <span
-      class="n-transition transition bg-[#F6F7F5] transition-300 absolute top-3 peer-checked:bg-green-300 peer-checked:border-transparent left-0 h-6 w-6 rounded-md border border-gray-500 peer-checked:rotate-90 peer-checked:border-green peer-checked:after:opacity-100 peer-checked:after:rotate-[-48deg] after:transition-all after:duration-200 after:absolute after:left-[5.5px] after:top-[5px] after:w-1.5 after:h-[11px] after:border-r-[2.2px] after:border-b-[2.2px] after:border-white after:rotate-[90deg] after:opacity-0 group-hover:border-green peer-disabled:border-grey-100 peer-disabled:after:border-grey-100"
+      class="n-transition transition bg-[#F6F7F5] transition-300 absolute top-3 peer-checked:bg-green-300 peer-checked:border-transparent left-0 h-6 w-6 rounded-md border border-gray-500 peer-checked:border-green peer-checked:after:opacity-100 after:rotate-45 after:transition-all after:duration-200 after:absolute after:top-[5px] after:w-1.5 after:h-[11px] after:border-r-[2.2px] after:border-b-[2.2px] after:border-white after:left-2 after:opacity-0 group-hover:border-green peer-disabled:border-grey-100 peer-disabled:after:border-grey-100"
       :class="{ '!border-red': error }"
     />
     <span class="pl-8 max-w-[301px] text-gray-200  sm:text-base text-sm leading-130">
