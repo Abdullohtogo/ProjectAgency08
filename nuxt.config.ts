2bc93aa839ff270dbcef8d32a2f157568be78a6c
Elzodxon <e.sharofaddinov@uic.group>
2023-08-25 18:12:30 +0500

    feat: smart banner

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
             ],
             script: [
                 {
-                    src: 'https://cdnjs.cloudflare.com/ajax/libs/smartbanner.js/1.14.6/smartbanner.min.js',
+                    src: '/smartbanner.min.js',
                 },
             ],
             meta: [
