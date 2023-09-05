c3703cbe939daae37997295dbcb4dadf839effff
T.Bobur <b.turabov@imkon.uz>
2023-09-05 16:11:13 +0500

    feat: header position to fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <LayoutHeader />
-    <div>
+    <LayoutHeader class="fixed w-full z-[1000000]" />
+    <div class="md:pt-[88px] pt-[72px]">
       <slot></slot>
     </div>
     <LayoutFooter />
