1e4a43c2bfb80ba953ccf2bdacdb2af196815e2c
Axadov Sherzod <sh.axadov@uic.group>
2023-09-09 15:10:53 +0500

    fix: coming soon

--- components/Common/Checkbox.vue
+++ components/Common/Checkbox.vue
@@ -16,13 +16,10 @@
       class="n-transition transition bg-[#F6F7F5] transition-300 absolute top-3 peer-checked:bg-green-300 peer-checked:border-transparent left-0 h-6 w-6 rounded-md border border-gray-500 peer-checked:border-green peer-checked:after:opacity-100 after:rotate-45 after:transition-all after:duration-200 after:absolute after:top-[5px] after:w-1.5 after:h-[11px] after:border-r-[2.2px] after:border-b-[2.2px] after:border-white after:left-2 after:opacity-0 group-hover:border-green peer-disabled:border-grey-100 peer-disabled:after:border-grey-100"
       :class="{ '!border-red': error }"
     />
-    <span class="pl-8 max-w-[301px] text-gray-200  sm:text-base text-sm leading-130">
-      <slot class="">
-        <a :href="url" target="_blank" class="text-[#4489F7] font-medium">
-          {{ $t(labelStart) }}
-        </a>
-        {{ $t(label) }}
-      </slot>
+    <span
+      class="pl-8 max-w-[301px] text-gray-200 sm:text-base text-sm leading-130"
+    >
+      <slot></slot>
     </span>
   </label>
 </template>
@@ -31,8 +28,8 @@
 export interface Props {
   modelValue?: any
   label?: string
-  labelStart: string
-  url: string
+  labelStart?: string
+  url?: string
   name?: string
   value?: any
   disabled?: boolean
