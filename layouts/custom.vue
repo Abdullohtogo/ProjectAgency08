48828b5eb2d2f0c6a92bc5e1e95d46a6ada5fe11
Axadov Sherzod <sh.axadov@uic.group>
2023-09-09 17:42:39 +0500

    fix: responsive

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <LayoutHeader :white="true" class="fixed  w-full z-[1000000]" />
+    <LayoutHeader :white="true" class="fixed w-full z-[1000000]" />
     <div class="md:pt-[88px] pt-[72px]">
       <slot></slot>
     </div>
-    <LayoutFooter />
+    <!--    <LayoutFooter />-->
   </div>
 </template>
