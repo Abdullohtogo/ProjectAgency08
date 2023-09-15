cd67f44de1755b654041eaf4e3877fe683d35a78
Sanjar Barakayev <s.barakayev@imkon.uz>
2023-09-15 16:19:48 +0500

    feat: update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps: [
         {
-            name: 'Commeta Hissa Landing',
-            port: 3098,
+            name: 'Satori Businiess Panel',
+            port: 3102,
             exec_mode: 'cluster',
             instances: '1',
             script: './.output/server/index.mjs',
