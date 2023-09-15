cb531234bd76442935fe48591133a0c8d443b4e7
Sanjar Barakayev <s.barakayev@imkon.uz>
2023-09-15 15:37:31 +0500

    fix: remove coming soon

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="h-full">
-    <SectionComingSoon />
+<!--    <SectionComingSoon />-->
+    <SectionMainIntro class="mb-7 mt-6" />
+    <SectionProjectAim id="about" class="lg:mb-12 md:mb-8 mb-6" />
+    <SectionMainCards id="afzalliklar" class="lg:pb-10 md:pb-8 pb-6" />
+    <SectionProjectsBlock class="my-8 md:my-16" />
+    <SectionAppBanner id="ilova" />
+    <SectionPartners id="fonds" @scroll="scrollToSection()" class="mb-10" />
+    <SectionContactUs id="contact" class="mb-[96px]" />
   </div>
-  <!-- <SectionMainIntro class="mb-7 mt-6" />
-  <SectionProjectAim id="about" class="lg:mb-12 md:mb-8 mb-6" />
-  <SectionMainCards id="afzalliklar" class="lg:pb-10 md:pb-8 pb-6" />
-  <SectionProjectsBlock class="my-8 md:my-16" />
-  <SectionAppBanner id="ilova" />
-  <SectionPartners id="fonds" @scroll="scrollToSection()" class="mb-10" />
-  <SectionContactUs id="contact" class="mb-[96px]" /> -->
-  <div></div>
 </template>
+
 <script lang="ts" setup>
+import {definePageMeta} from "#imports";
+
 function scrollToSection() {
   const section = document.getElementById('contact')
+
+  if (!section) {
+    return
+  }
+
   section.scrollIntoView({
     behavior: 'smooth',
     inline: 'center',
     block: 'center',
   })
 }
+
 definePageMeta({
   layout: 'custom',
 })
