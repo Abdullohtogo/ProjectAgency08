b60a854244687c9d55cb96946e83c157062bab81
Axadov Sherzod <sh.axadov@uic.group>
2023-09-18 12:50:29 +0500

    fix: header menu

--- components/Layout/Header/Header.vue
+++ components/Layout/Header/Header.vue
@@ -8,48 +8,48 @@
       :class="white ? 'md:py-8 sm:py-7 py-6' : 'md:py-6 sm:py-5 py-4'"
       class="container flex justify-between items-center sticky z-50"
     >
-      <!--      <button-->
-      <!--        class="md:hidden block hover:cursor-pointer w-7 h-7"-->
-      <!--        @click="showMenu = !showMenu"-->
-      <!--      >-->
-      <!--        <Transition mode="out-in" name="fade">-->
-      <!--          <svg-->
-      <!--            v-if="!showMenu"-->
-      <!--            width="28"-->
-      <!--            height="28"-->
-      <!--            viewBox="0 0 28 28"-->
-      <!--            fill="none"-->
-      <!--            xmlns="http://www.w3.org/2000/svg"-->
-      <!--          >-->
-      <!--            <g id="burger">-->
-      <!--              <path-->
-      <!--                id="Icon"-->
-      <!--                d="M4.66666 7H23.3333M4.66666 14H16.3333M4.66666 21H10.5"-->
-      <!--                stroke="black"-->
-      <!--                stroke-width="2"-->
-      <!--                stroke-linecap="round"-->
-      <!--                stroke-linejoin="round"-->
-      <!--              />-->
-      <!--            </g>-->
-      <!--          </svg>-->
-      <!--          <i v-else class="icon-close text-[28px] leading-7 text-dark" />-->
-      <!--        </Transition>-->
-      <!--      </button>-->
+      <button
+        class="md:hidden block hover:cursor-pointer w-7 h-7"
+        @click="showMenu = !showMenu"
+      >
+        <Transition mode="out-in" name="fade">
+          <svg
+            v-if="!showMenu"
+            width="28"
+            height="28"
+            viewBox="0 0 28 28"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="burger">
+              <path
+                id="Icon"
+                d="M4.66666 7H23.3333M4.66666 14H16.3333M4.66666 21H10.5"
+                stroke="black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </svg>
+          <i v-else class="icon-close text-[28px] leading-7 text-dark" />
+        </Transition>
+      </button>
       <div>
         <router-link to="/">
           <img src="../../../public/logo.svg" alt="logo" />
         </router-link>
       </div>
-      <!--      <ul class="gap-4 lg:gap-8 hidden md:flex">-->
-      <!--        <li v-for="item in menu" :key="item.id">-->
-      <!--          <button-->
-      <!--            @click="scrollTo(item.url)"-->
-      <!--            class="hover:text-green-400 text-sm leading-5 text-black-100 transition-all duration-300 ease-linear font-medium"-->
-      <!--          >-->
-      <!--            {{ $t(item.text) }}-->
-      <!--          </button>-->
-      <!--        </li>-->
-      <!--      </ul>-->
+      <ul class="gap-4 lg:gap-8 hidden md:flex">
+        <li v-for="item in menu" :key="item.id">
+          <button
+            @click="scrollTo(item.url)"
+            class="hover:text-green-400 text-sm leading-5 text-black-100 transition-all duration-300 ease-linear font-medium"
+          >
+            {{ $t(item.text) }}
+          </button>
+        </li>
+      </ul>
       <UILanguageSwitcher v-bind="{ variant: 'default' }" />
     </div>
     <LayoutHeaderFullMenu
