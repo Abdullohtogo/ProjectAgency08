b9dc204a0d2ce45fd0683a979868bd7e536178a7
Axadov Sherzod <sh.axadov@uic.group>
2023-10-04 11:33:44 +0500

    fix: locales

--- config.ts
+++ config.ts
@@ -2,5 +2,4 @@ export const CONFIG = {
   ID_DOMAIN: import.meta.env.VITE_APP_ID_URL,
   BASE_URL: import.meta.env.VITE_API_BASE_URL,
   APP_URL: import.meta.env.VITE_APP_APPLICATION_URL,
-  BASE_LANG_URL: import.meta.env.VITE_API_BASE_LANG_URL,
 }
